2c205b0cb17f7058aa8d4605aa71e69e796a627c
Mengwai Chan <chanmengwai1@gmail.com>
2024-02-15 01:39:53 -0500

    fix: discord, calendar

--- main.py
+++ main.py
@@ -13,17 +13,16 @@ def main():
     question = LeetCode().fetch_question()
     question = json.loads(question)
 
-    #event = GoogleCalendar(os.getenv("GOOGLE_CAL_ID"))
-    #event.load_credentials()
-    #event.create_event(question)
+    event = GoogleCalendar(os.getenv("GOOGLE_CAL_ID"))
+    event.load_credentials()
+    event.create_event(question)
 
-    #discord = Discord(os.getenv('DISCORD_URL'))
-    #discord.send_message(question)
+    discord = Discord(os.getenv('DISCORD_URL'))
+    discord.send_message(question)
     
     db = Firebase()
     db.init_cred()
     db.insert_question(question)
-
 
 if __name__ == "__main__":
     main()
